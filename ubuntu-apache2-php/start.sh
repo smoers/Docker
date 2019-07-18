c40c89247a6bb40a574d40279fcb5ac11b3bd350
smoers <serge.moers@gmail.com>
2019-07-18 16:16:40 +0200

    SNAE 2019-07-18-02

--- ubuntu-apache2-php/start.sh
+++ ubuntu-apache2-php/start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 /usr/bin/supervisord
+
+if [ $1 != "" ]; then
+        mysql -u root -e "create database phpipam;"
+        mysql -u root -e "grant all on phpipam.* to phpipam@localhost identified by '$1';"
+fi
+
